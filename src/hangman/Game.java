7f522871e080783a5f57be6df2a788dc8a5708b8
bohac <honza.boh00@gmail.com>
2024-06-03 11:06:59 +0200

    cv, cykly A2

--- src/hangman/Game.java
+++ src/hangman/Game.java
@@ -22,12 +22,39 @@ public class Game {
     void setupGame(){
         p.resetAttempts();
         checked = new ArrayList<>();
-
+        secret = pickRandomWord();
     }
 
     String pickRandomWord(){
         int randomIndex = (int) (Math.random() * POOL.length);
         System.out.println("CHEAT: " + POOL[randomIndex]);
         return POOL[randomIndex];
+    }
+
+
+
+    void makeAGuess(){
+        System.out.println("Pokusu zbyva: " + p.attempts);
+        System.out.println("Zadej pismeno k uhadnuti: ");
+        String guess = inputReader.readInput();
+        if (secret.contains(guess)){
+            System.out.println("Pismeno uhadnuto!");
+            checked.add(guess);
+        } else {
+            System.out.println("Nespravna volba");
+
+        }
+    }
+
+    void startGame(){
+        System.out.println("Spustit hru?");
+        if (inputReader.readInput().equals("y")){
+            setupGame();
+            System.out.println("Hra zacala");
+            while(p.attempts > 0 && checked.size() < secret.length()){
+                makeAGuess();
+            }
+        }
+        System.out.println("Hra ukoncena");
     }
 }
